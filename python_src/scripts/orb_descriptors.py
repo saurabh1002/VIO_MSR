1ba4cfe5e35d5e176dff8fc40d4d7b755b523dd3
Saurabh Gupta <saurabh.gupta1002@gmail.com>
2021-12-10 15:21:42 +0100

    ORB descriptor matching

--- python_src/scripts/orb_descriptors.py
+++ python_src/scripts/orb_descriptors.py
@@ -26,7 +26,7 @@ def display_imgs(im_dict: dict, wait_val: int = 0, new_window: bool = False):
         cv2.destroyAllWindows()
 
 
-img_dataset_path = '../../datasets/phenorob/mapping/images_apples/rgb_for_vocab/'
+img_dataset_path = '../../datasets/phenorob/images_apples/rgb_for_vocab/'
 
 orb = cv2.ORB_create(scoreType=cv2.ORB_FAST_SCORE, fastThreshold=5)
 
@@ -45,20 +45,11 @@ for i in range(1, NUM_IMAGES):
 
     img_rgb_b = cv2.imread(img_dataset_path + 'image{}.png'.format(i + step))
     img_bgr_b = cv2.cvtColor(img_rgb_b, cv2.COLOR_RGB2BGR)
-
     
     keypoints_a, descriptors_a = orb.detectAndCompute(img_bgr_a, None)
     keypoints_b, descriptors_b = orb.detectAndCompute(img_bgr_b, None)
     
     num_of_keypoints[i] = len(keypoints_a)
-
-    # img_corners_a = np.zeros_like(img_bgr_a)
-    # cv2.drawKeypoints(img_bgr_a, keypoints_a, img_corners_a, color=(255,0,0))
-
-    # img_corners_b = np.zeros_like(img_bgr_b)
-    # cv2.drawKeypoints(img_bgr_b, keypoints_b, img_corners_b, color=(255,0,0))
-
-    # display_imgs({'Fast corners a': img_corners_a, 'Fast corners b': img_corners_b}, 0)
 
     matcher = cv2.BFMatcher_create(cv2.NORM_HAMMING, True)
     matches = matcher.match(descriptors_a, descriptors_b)
@@ -76,10 +67,6 @@ for i in range(1, NUM_IMAGES):
     cv2.imshow('matches', img_match)
     cv2.waitKey(0)
     # cv2.imwrite('tmp/img_{}.png'.format(i), img_match)
-    
-# display_imgs({'ORB Feature Matches', img_match}, 0)
-print(np.min(num_of_keypoints), np.argmin(num_of_keypoints), np.max(num_of_keypoints))
-print(np.min(num_of_matches), np.argmin(num_of_matches), np.max(num_of_matches))
 
 fig, (ax1, ax2) = plt.subplots(1, 2)
 ax1.hist(num_of_keypoints)
